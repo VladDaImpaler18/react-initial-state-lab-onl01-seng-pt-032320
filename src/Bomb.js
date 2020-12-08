87bbd709507495f0dd11a5e67b1e256c4c0ee1ea
VladDaImpaler18 <vladDaImpaler@gmail.com>
2020-12-07 19:42:31 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,12 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+
+class Bomb extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default Bomb
